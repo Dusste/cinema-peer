0d570733580b54efab4df3d4f77b579938f38819
Dusan Stevanovic <dusan.stevanovic@kindredgroup.com>
2023-10-25 19:39:21 +0200

    Trash api keys

--- src/Env.elm
+++ src/Env.elm
@@ -1,33 +1,29 @@
 module Env exposing (..)
 
--- The Env.elm file is for per-environment configuration.
--- See https://dashboard.lamdera.app/docs/environment for more info.
-
 
 apiToken =
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjQ5ZGI1MjY3NDIzYjkzMTdmN2IwMTkyNDNmZjYwZCIsInN1YiI6IjY1MGVkN2RlNmY1M2UxMGFhYzJhNDIzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UHT56I6JHIMa7VIjLAGbRCmeLtqffSFK8IDjmqc_pq0"
+    "muppet"
 
 
 apiKey =
-    "e249db5267423b9317f7b019243ff60d"
+    "muppet"
 
 
 localhost =
-    "8000"
+    "muppet"
 
 
 salt =
-    "kmk3omomdkmasowij93e9j1239i1ncsjndsai12921d0nsad093290231jdm9023k90923mw0"
+    "muppet"
 
 
 sendGridApiKey =
-    "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"
+    "muppet"
 
 
 rawStringForSGapiKey =
-    "Imdsakmas;d;asmk'masmkdasmkd32909021j421mdksa;mkda"
+    "muppet"
 
 
 senderEmail =
-    -- "no-reply@cinema-peer.com"
-    "blunthomebrew@gmail.com"
+    "no-reply@cinema-peer.com"
